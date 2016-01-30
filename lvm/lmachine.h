8e1a84bc53265dd372e41a047916d78e041b2ba4
Leviathan <Leviathan1995@outlook.com>
2016-01-30 22:15:20 +0800

    Update lmachine.h

--- lvm/lmachine.h
+++ lvm/lmachine.h
@@ -7,8 +7,8 @@
 //
 
 #pragma once
-#ifndef _Lvm_lmachine_
-#define _Lvm__lmachine_
+#ifndef LMACHINE_H_
+#define LMACHINE_H_
 
 #include <iostream>
 #include <vector>
@@ -47,4 +47,4 @@ public:
     static string outfile;
     
 };
-#endif /* defined(__Lvm__lmachine__) */
+#endif /* defined(LMACHINE_H_) */
